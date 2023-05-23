d8ab914b22515849ed6ad64efd9d9269ad5bb43a
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2023-05-22 21:51:44 -0600

    so

--- src/features/auth/auth.js
+++ src/features/auth/auth.js
@@ -1,28 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  userToken: null,
-  isLoading: true,
-  isSignout: true,
-};
+const initialState = {};
 
 const authSlice = createSlice({
-  name: "auth",
-  initialState: initialState,
-  reducers: {
-    restoreToken: (state, action) => {
-      state.userToken = action.payload;
-      state.isLoading = false;
-    },
-    signIn: (state, action) => {
-      state.isSignout = false;
-      state.userToken = action.payload;
-    },
-    signOut: (state) => {
-      state.isSignout = true;
-      state.userToken = null;
-    },
-  },
+  name: "schedules",
 });
 
 export const { restoreToken, signIn, signOut } = authSlice.actions;
