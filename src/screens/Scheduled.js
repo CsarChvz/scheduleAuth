676da67cb7fc59deea2bdb83122b5e70526dac2c
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2023-05-23 13:35:14 -0600

    Calendar added

--- src/screens/Scheduled.js
+++ src/screens/Scheduled.js
@@ -2,12 +2,25 @@ import React from "react";
 import { View, Text } from "react-native";
 import { globalStyles } from "../styles/globalStyles";
 import { useNavigation } from "@react-navigation/native";
+import { Calendar, LocaleConfig } from "react-native-calendars";
 
 function Scheduled() {
   const navigation = useNavigation();
+  const [selected, setSelected] = React.useState("");
   return (
     <View style={globalStyles.container}>
-      <Text>Scheduled</Text>
+      <Calendar
+        onDayPress={(day) => {
+          setSelected(day.dateString);
+        }}
+        markedDates={{
+          [selected]: {
+            selected: true,
+            disableTouchEvent: true,
+            selectedDotColor: "orange",
+          },
+        }}
+      />
     </View>
   );
 }
